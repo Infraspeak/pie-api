f84228e82c6eeee566181ecebc637380fc87fbf6
Bessa <lbessa@infraspeak.com>
2020-10-23 17:25:27 +0100

    add publish to file request

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -5,40 +5,42 @@ namespace App\Http\Controllers;
 use App\Http\Requests\FileRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Redis;
 
 class FilesController extends Controller
 {
     public function store(FileRequest $request): JsonResponse
     {
         $file = $request->file('file');
-        $fileType = $this->getFileType($file->getClientOriginalName());
+        $brokerType = $this->getBrokerType($file->getClientOriginalName());
 
-        if (!$fileType) {
+        if (!$brokerType) {
             return response()->json(['error' => 'Invalid file'], Response::HTTP_BAD_REQUEST);
         }
 
         $brokerPayload = [
-            'file' => $fileType,
             'headers' => [
                 'uuid' => $request->get('uuid'),
             ],
             'payload' => file_get_contents($file->getRealPath())
         ];
 
+        Redis::publish($brokerType, $brokerPayload);
+
         return response()->json([], 200);
     }
 
-    protected function getFileType($file): string
+    protected function getBrokerType($fileName): string
     {
-        switch ($file) {
+        switch ($fileName) {
             case 'composer.json':
-                $fileType = 'COMPOSER_FILE';
+                $brokerType = 'COMPOSER_FILE';
                 break;
             case 'package.json':
-                $fileType = 'NPM_FILE';
+                $brokerType = 'NPM_FILE';
                 break;
         }
 
-        return $fileType;
+        return $brokerType;
     }
 }
